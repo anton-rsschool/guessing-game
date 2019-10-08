330a9af29d696a35e973e941ea8e1c764c2c55ec
Anthon_M <xll2005@tut.by>
2019-10-08 10:24:43 +0300

    feat: add task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,27 @@
 class GuessingGame {
-    constructor() {}
+  constructor() {
+  	this._min = null;
+  	this._max = null;
+  	this._average = null;
+  }
 
-    setRange(min, max) {
+  setRange(min, max) {
+  	this._min = min + 1;
+  	this._max = max;
+  }
 
-    }
+  guess() {
+  	this._average = Math.floor((this._max + this._min) / 2);
+  	return this._average;
+  }
 
-    guess() {
+  lower() {
+  	this._max = this._average;
+  }
 
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+  greater() {
+  	this._min = this._average + 1;
+  }
 }
 
 module.exports = GuessingGame;
